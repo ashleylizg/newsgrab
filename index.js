4d79135f808c2cb1d4b135cad62e372c2574ed97
Ashley Gingeleski <agingeleski@gmail.com>
2020-06-09 19:52:13 -0400

    some code cleanup

--- index.js
+++ index.js
@@ -1,24 +1,62 @@
+const fs = require('fs');
+
+
 const { chromium } = require('playwright');
 
 const MODAL_BUTTON_SELECTOR = '.modal-footer > button';
 const SEARCH_SELECTOR = 'input[placeholder=Search]';
 const LOCATION_SELECTOR = 'li.active > a';
 
-const GOOGLE_NEWS_URL = 'https://www.google.com/search?q=wells+college&source=lnms&tbm=nws';
-const GOOGLE_RESULTS_SELECTOR = 'div[data-async-context="query:wells%20college"] > div > g-card div[role="heading"]';
+const GOOGLE_NEWS_URL = 'https://www.google.com/search?q=COLLEGE&source=lnms&tbm=nws';
+const GOOGLE_RESULTS_SELECTOR = 'div[data-async-context="query:COLLEGE"] > div > g-card div[role="heading"]';
 
-(async () => {
-  const browser = await chromium.launch();
-  const page = await browser.newPage();
+fs.readFileSync('ny_colleges.txt').toString().split("\r\n").forEach(function(line, index, arr)
+{
+  if (index === arr.length - 1 && line === '') { return; }
 
-  await page.goto(GOOGLE_NEWS_URL);
+  let collegeName = line;
+  console.log('Looking at ' + collegeName);
 
-  let results = await page.$$(GOOGLE_RESULTS_SELECTOR);
+  // Craft search URL - remove special characters, remove carriage returns
+  let thisNewsUrl = GOOGLE_NEWS_URL.replace('COLLEGE', collegeName.replace(/[^a-zA-Z ]/g, '').replace(/\s+/g,'+'));
+  console.log(thisNewsUrl);
 
-  for (var i = 0; i < results.length; i += 1) {
-    var thisResultsTitle = await (await results[i].getProperty('innerHTML')).jsonValue();
-    console.log(thisResultsTitle);
-  }
+  // Craft JavaScript selector for search results webpage
+  let thisResultsSelector = GOOGLE_RESULTS_SELECTOR.replace('COLLEGE', encodeURIComponent(collegeName));
+  console.log(thisResultsSelector);
 
-  await browser.close();
-})();
+  // TODO logic for this college - still not sure about looping this and writing it, if functions are built correctly
+ 
+  (async () => {
+    const browser = await chromium.launch();
+    const page = await browser.newPage();
+  
+    await page.goto(GOOGLE_NEWS_URL);
+  
+    let results = await page.$$(GOOGLE_RESULTS_SELECTOR);
+  //added below line
+    let writeStream = fs.createWriteStream('output.txt');
+  
+  
+    for (var i = 0; i < results.length; i += 1) {
+      var thisResultsTitle = await (await results[i].getProperty('innerHTML')).jsonValue();
+      console.log(thisResultsTitle);
+      //does this seem okay? added the below lines in to write to a file
+      writeStream.write(thisResultsTitle);
+      writeStream.on('finish', () => {
+        console.log('wrote all data to file');
+    });
+    
+    // close the stream
+      writeStream.end();  
+  
+    }
+  
+    await browser.close();
+  })(); 
+
+});
+console.log('End of processing');
+
+// End the program
+process.exit(0);
