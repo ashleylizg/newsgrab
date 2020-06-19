a310f45c865a641b9ff34218da05b41ea1318862
Randy Gingeleski <4685844+gingeleski@users.noreply.github.com>
2020-06-19 18:05:02 -0400

    fix async headless browser stuff

--- index.js
+++ index.js
@@ -1,95 +1,56 @@
 const fs = require('fs');
 
-
 const { chromium } = require('playwright');
 
+// CONSTANTS
+
+const GOOGLE_NEWS_URL = 'https://www.google.com/search?q=COLLEGE&source=lnms&tbm=nws';
+const GOOGLE_RESULTS_SELECTOR = 'div[data-async-context="query:COLLEGE"] > div > g-card div[role="heading"]';
 const MODAL_BUTTON_SELECTOR = '.modal-footer > button';
 const SEARCH_SELECTOR = 'input[placeholder=Search]';
 const LOCATION_SELECTOR = 'li.active > a';
 
-const GOOGLE_NEWS_URL = 'https://www.google.com/search?q=COLLEGE&source=lnms&tbm=nws';
-const GOOGLE_RESULTS_SELECTOR = 'div[data-async-context="query:COLLEGE"] > div > g-card div[role="heading"]';
+// MAIN PROGRAM LOGIC
 
-const listOfColleges = fs.readFileSync('ny_colleges.txt').toString().split("\r\n");
+let listOfSearchTerms = fs.readFileSync('ny_colleges.txt').toString().split('\n');
 
-// try an async function here using the below code
+// Will loop through search terms inside async function - need async for Playwright browser stuff
+(async function() {
+  console.log('Start of async processing');
 
-
-
-
-for (let index = 0; index < listOfColleges.length; index +=1 ) {
-  let line = listOfColleges[index];
-  if (line === '') continue;
-
-  let collegeName = line;
-  console.log('Looking at ' + collegeName);
-
-  // Craft search URL - remove special characters, remove carriage returns
-  let thisNewsUrl = GOOGLE_NEWS_URL.replace('COLLEGE', collegeName.replace(/[^a-zA-Z ]/g, '').replace(/\s+/g,'+'));
-  console.log(thisNewsUrl);
-
-  // Craft JavaScript selector for search results webpage
-  let thisResultsSelector = GOOGLE_RESULTS_SELECTOR.replace('COLLEGE', encodeURIComponent(collegeName));
-  console.log(thisResultsSelector);
-
-  // TODO logic for this college - still not sure about looping this and writing it, if functions are built correctly
- async function main () {
+  // Set up headless browser to use then re-use
   const browser = await chromium.launch();
   const page = await browser.newPage();
 
-  await page.goto(GOOGLE_NEWS_URL);
-  let results = await page.$$(GOOGLE_RESULTS_SELECTOR).waitForNavigation();
-//added below line
-  let writeStream = fs.createWriteStream('./output.txt');
-//added below line and closed following write stream
-  for (var i = 0; i < results.length; i += 1) {
-    var thisResultsTitle = await (await results[i].getProperty('innerHTML')).jsonValue();
-    console.log(thisResultsTitle);
-    //does this seem okay? added the below lines in to write to a file
-    writeStream.write(thisResultsTitle);
-    writeStream.on('finish', () => {
-      console.log('wrote all data to file');
-  });
+  for (let i = 0; i < listOfSearchTerms.length; i +=1 ) {
+    console.log('Loop at line ' + i);
+    let line = listOfSearchTerms[i];
+    if (line === '') continue;
   
-  // close the stream
-    writeStream.end();  
+    let searchterm = line;
+    console.log('Looking up search term -> ' + searchterm);
 
-  }
-
-  await browser.close();
-
-  console.log('End of processing');
- }
-
-
-/*
-  (async () => {
-    const browser = await chromium.launch();
-    const page = await browser.newPage();
+    // Craft search URL - remove special characters, remove carriage returns
+    let thisNewsUrl = GOOGLE_NEWS_URL.replace('COLLEGE', searchterm.replace(/[^a-zA-Z ]/g, '').replace(/\s+/g,'+'));
+    console.log(thisNewsUrl);
+    
+    // Craft JavaScript selector for search results webpage
+    let thisResultsSelector = GOOGLE_RESULTS_SELECTOR.replace('COLLEGE', encodeURIComponent(searchterm));
+    console.log(thisResultsSelector);
   
     await page.goto(GOOGLE_NEWS_URL);
-    let results = await page.$$(GOOGLE_RESULTS_SELECTOR).waitForNavigation();
-  //added below line
-    let writeStream = fs.createWriteStream('./output.txt');
-  //added below line and closed following write stream
-    for (var i = 0; i < results.length; i += 1) {
-      var thisResultsTitle = await (await results[i].getProperty('innerHTML')).jsonValue();
-      console.log(thisResultsTitle);
-      //does this seem okay? added the below lines in to write to a file
-      writeStream.write(thisResultsTitle);
-      writeStream.on('finish', () => {
-        console.log('wrote all data to file');
-    });
-    
-    // close the stream
-      writeStream.end();  
+    let results = await page.$$(GOOGLE_RESULTS_SELECTOR);
   
+    for (var j = 0; j < results.length; j += 1) {
+      var thisResultsTitle = await (await results[j].getProperty('innerHTML')).jsonValue();
+      console.log(thisResultsTitle);
     }
+  }
 
-    await browser.close();
-  })(); 
-}
-*/
-main()
-  // End the program
-process.exit(0);
+  // TODO write results here
+
+  // Close browser now that we have gone through all search terms
+  await browser.close();
+
+  console.log('End of async processing');
+})();
