3e73117ba2a5896c6bfe5bd501e4d8385930ea60
Ashley Gingeleski <agingeleski@gmail.com>
2020-07-15 14:26:18 -0400

    adds annotations to index.js and reformats example data

--- index.js
+++ index.js
@@ -4,30 +4,31 @@ const { chromium } = require('playwright');
 
 // CONSTANTS
 
-const GOOGLE_NEWS_URL = 'https://www.google.com/search?q=COLLEGE&source=lnms&tbm=nws';
-const GOOGLE_RESULTS_SELECTOR = 'div[data-async-context="query:COLLEGE"] > div > g-card div[role="heading"]';
+const GOOGLE_NEWS_URL = 'https://www.google.com/search?q=TERM&source=lnms&tbm=nws';
+const GOOGLE_RESULTS_SELECTOR = 'div[data-async-context="query:TERM"] > div > g-card div[role="heading"]';
 const MODAL_BUTTON_SELECTOR = '.modal-footer > button';
 const SEARCH_SELECTOR = 'input[placeholder=Search]';
 const LOCATION_SELECTOR = 'li.active > a';
 
 // MAIN PROGRAM LOGIC
 
-let listOfSearchTerms = fs.readFileSync('sample.txt').toString().split(',');
+let listOfSearchTerms = fs.readFileSync('file.txt').toString().split(',');
 
-//create an array to append the results to
+//Create an array to append the results to
 let searchResultsList = [];
 
 // Will loop through search terms inside async function - need async for Playwright browser stuff
 (async function() {
   console.log('Start of async processing');
 
-  //initialize object that will be appended to array
+  // Initialize object that will be appended to array
   let searchResultData = {};
 
   // Set up headless browser to use then re-use
   const browser = await chromium.launch();
   const page = await browser.newPage();
 
+  // Initiate search project by each term
   for (let i = 0; i < listOfSearchTerms.length; i += 1) {
     console.log('Loop at line ' + i);
     let line = listOfSearchTerms[i];
@@ -36,31 +37,34 @@ let searchResultsList = [];
     let searchterm = line;
     console.log('Looking up search term -> ' + searchterm);
 
-    //create object for search term
+    // Create object for search term
     searchResultData = {};
     searchResultData.search_term = searchterm;
 
-    // Craft search URL - remove special characters, remove carriage returns
-    let thisNewsUrl = GOOGLE_NEWS_URL.replace('COLLEGE', searchterm.replace(/[^a-zA-Z ]/g, '').replace(/\s+/g,'+'));
+    // Craft search URL - remove special characters
+    let thisNewsUrl = GOOGLE_NEWS_URL.replace('TERM', searchterm.replace(/[^a-zA-Z ]/g, '').replace(/\s+/g,'+'));
     console.log(thisNewsUrl);
 
     // Craft JavaScript selector for search results webpage
-    let thisResultsSelector = GOOGLE_RESULTS_SELECTOR.replace('COLLEGE', encodeURIComponent(searchterm));
+    let thisResultsSelector = GOOGLE_RESULTS_SELECTOR.replace('TERM', encodeURIComponent(searchterm));
     console.log(thisResultsSelector);
     
-    //loops through too quickly and only grabs last college
+    // Go to the URL for each search term
     await page.goto(thisNewsUrl);
     let results = await page.$$(thisResultsSelector);
   
+    // Create an array for the term's results
     let thisResultsTitle = '';
     searchResultData.results = [];
 
+    // Append the term's results to the search result data array
     for (let j = 0; j < results.length; j += 1) {
       thisResultsTitle = await (await results[j].getProperty('innerHTML')).jsonValue();
       console.log(thisResultsTitle);
       searchResultData.results.push(thisResultsTitle);
     }
 
+    // Push all term results to complete data array
     searchResultsList.push(searchResultData);
     console.log(searchResultData);
   }
@@ -70,10 +74,11 @@ let searchResultsList = [];
 
   console.log('End of async processing');
 
-  // create encode json string
+  // Create encoded json string
   let searchResultsListJson = JSON.stringify(searchResultsList);
   console.log(searchResultsListJson);
-  //write json output to file
+  
+  // Write json output to file
   fs.writeFile("output.json", searchResultsListJson, 'utf8', function (err) {
     if (err) {
         console.log("An error occured while writing JSON Object to File.");
